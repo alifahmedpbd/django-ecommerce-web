72f9e7c6407153a611c5f063d3955eb1d3d0f5df
alif275 <alifahmed200022@gmail.com>
2026-06-28 11:42:35 +0600

    Create project applications

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'core',
+    'store',
+    'cart',
+    'orders',
+    'payments',
 ]
 
 MIDDLEWARE = [
