dd31ae605c424c9d6136b323224c1e430042fbd4
alif275 <alifahmed200022@gmail.com>
2026-07-01 12:19:00 +0600

    Custom User model & Implement user registration and login

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ("customer", "Customer"),
+        ("seller", "Seller"),
+        ("admin", "Admin"),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="customer")
+    phone = models.CharField(max_length=15, blank=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from urllib import request
 
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserRegisterForm
 # Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+
+            messages.success(request, "Account Created Successfully!")
+            return redirect("home")
+    else:
+        form = UserRegisterForm()
+    return render(request, "accounts/register.html", {"form": form})
+
+def login_view(request):
+
+    if request.method == "POST":
+
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+
+            return redirect("home")
+        
+        messages.error(request, "Invalid username or password.")
+
+    return render(request, "accounts/login.html")
+
+def logout_view(request):
+
+    logout(request)
+    return redirect("home")
+
