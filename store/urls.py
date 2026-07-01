e34ac1273a7af39afd1f7afc57d71ea5ad1c330d
alif275 <alifahmed200022@gmail.com>
2026-07-01 14:47:30 +0600

    Add product list page

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = "store"
+
+urlpatterns = [
+
+    path("products/", views.product_list, name="product_list"),
+
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Product, Category
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.filter(available=True)
+    
+    context = {
+        "products": products,
+    }
+
+    return render(request, "store/product_list.html", context)
