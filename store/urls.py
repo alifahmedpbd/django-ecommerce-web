b4b8ff19e89a2482706435665f9dd606a0f3ade3
alif275 <alifahmed200022@gmail.com>
2026-07-01 21:58:00 +0600

    Add category filtering and sidebar navigation

--- store/urls.py
+++ store/urls.py
@@ -8,6 +8,7 @@ app_name = "store"
 urlpatterns = [
 
     path("products/", views.product_list, name="product_list"),
+    path("category/<slug:category_slug>/", views.product_list, name="products_by_category"),
     path("products/<slug:slug>/", views.product_detail, name="product_detail"),
 
 ]
