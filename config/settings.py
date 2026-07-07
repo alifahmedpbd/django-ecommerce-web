d90d1e057ae9f0a3dc1da933bd43e8153b64a139
alif275 <alifahmed200022@gmail.com>
2026-07-07 17:46:05 +0600

    Fix production settings

--- config/settings.py
+++ config/settings.py
@@ -35,10 +35,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-]
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 
 
 # Application definition
