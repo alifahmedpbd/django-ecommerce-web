e252a0cf36b34751ce88fad595d2576c8a930ed4
alif275 <alifahmed200022@gmail.com>
2026-07-08 04:33:10 +0600

    Auto create production superuser

---
+++ create_superuser.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = "admin"
+email = "admin@example.com"
+password = "Admin12345"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password,
+    )
+    print("✅ Superuser created.")
+else:
+    print("✅ Superuser already exists.")
