c6c44b26278c413e551bf188946d6bc7978d75f6
alif275 <alifahmed200022@gmail.com>
2026-07-11 22:53:03 +0600

    create coupon models.

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,69 @@
 from django.db import models
 from accounts.models import User
 from store.models import Product
+from django.utils import timezone
 # Create your models here.
+
+
+# ==========================================
+# Coupon Model
+# ==========================================
+
+class Coupon(models.Model):
+
+    DISCOUNT_TYPE = (
+
+        ("percentage", "Percentage"),
+
+        ("fixed", "Fixed Amount"),
+
+    )
+
+    code = models.CharField(max_length=50, unique=True)
+    discount_type = models.CharField(max_length=20, choices=DISCOUNT_TYPE)
+    discount = models.DecimalField(max_digits=10, decimal_places=2)
+    minimum_purchase = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    usage_limit = models.PositiveIntegerField(default=1)
+    used_count = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    valid_from = models.DateTimeField(default=timezone.now)
+    valid_to = models.DateTimeField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        ordering = [
+
+            "-created_at",
+
+        ]
+
+    def __str__(self):
+
+        return self.code
+
+    @property
+    def is_expired(self):
+
+        return timezone.now() > self.valid_to
+
+    @property
+    def is_available(self):
+
+        return (
+
+            self.active
+
+            and
+
+            not self.is_expired
+
+            and
+
+            self.used_count < self.usage_limit
+
+        )
 
 class Order(models.Model):
 
@@ -29,12 +91,25 @@ class Order(models.Model):
     payment_method = models.CharField(max_length=200, choices=PAYMENT_METHODS, default="cod")
     payment_id = models.CharField(max_length=200, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    coupo = models.ForeignKey(Coupon, on_delete=models.CASCADE, null=True, blank=True)
+    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    final_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f"Order #{self.id}"
     
     def get_total_cost(self):
-        return sum(item.get_total_price() for item in self.items.all())
+
+        subtotal = sum(
+
+            item.get_total_price()
+
+            for item in self.items.all()
+
+        )
+
+        return subtotal - self.discount
+    
     
 class OrderItem(models.Model):
 
