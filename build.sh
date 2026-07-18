07265f3c31af6d3d1f2e1e2e7e2fa74c6bf4be52
alif275 <alifahmed200022@gmail.com>
2026-07-18 23:25:22 +0600

    Fix WhiteNoise static storage

--- build.sh
+++ build.sh
@@ -1,33 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Python Version:"
+python --version
+
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 export DJANGO_SETTINGS_MODULE=config.settings
 
 python manage.py check
-python manage.py collectstatic --noinput --clear
-
-
-#!/usr/bin/env bash
-set -o errexit
-
-echo "PWD:"
-pwd
-
-echo "FILES:"
-ls -la
-
-python --version
-
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-
-echo "DJANGO SETTINGS:"
-python -c "import config.settings as s; print(s.INSTALLED_APPS)"
-
-echo "MANAGE HELP:"
-python manage.py help
-
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear -v 3
